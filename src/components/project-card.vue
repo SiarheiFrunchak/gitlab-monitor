99b48d148ab7294d8597d692992a9a0400b42043
Siarhei Frunchak <siarhi.frunchak@rozum.com>
2019-10-16 11:20:01 +0300

    refactoring

--- src/components/project-card.vue
+++ src/components/project-card.vue
@@ -113,8 +113,6 @@
           if (branchesList != null){
             let listenBranches = branchesList.split(',')
 
-            console.log(memory)
-
             listenBranches.forEach(function(element, project=this.project, status=this.status){
               if (
                 pipelines &&
@@ -128,7 +126,6 @@
                   pipelines[element][0].status === 'failed'
                 ) {
                   if(!memory.includes(pipelines[element][0].id)){
-                    console.log(pipelines[element][0].id)
                     const alarmSound = new Audio(Config.root.linkToFailureSound)
                     const soundPromise = alarmSound.play()
                     if (soundPromise !== null){
@@ -155,14 +152,15 @@
             pipelines[configuredDefaultBranch].length > 0
           ) {
 
-            // if ( // Play sound alert if default branch status changes to failed
-            //   Config.root.linkToFailureSound != null &&
-            //   this.status !== 'failed' && !!this.status &&
-            //   pipelines[configuredDefaultBranch][0].status === 'failed'
-            // ) {
-            //   const alarmSound = new Audio(Config.root.linkToFailureSound)
-            //   alarmSound.play()
-            // }
+            if ( // Play sound alert if default branch status changes to failed
+              Config.root.listenBranches === null &&
+              Config.root.linkToFailureSound != null &&
+              this.status !== 'failed' && !!this.status &&
+              pipelines[configuredDefaultBranch][0].status === 'failed'
+            ) {
+              const alarmSound = new Audio(Config.root.linkToFailureSound)
+              alarmSound.play()
+            }
 
             this.status = pipelines[configuredDefaultBranch][0].status
 
